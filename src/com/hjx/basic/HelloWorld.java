e3e59af9d6bef184c31258c703e0dcc667c192ec
hjxjexjia <huangjiaxi_0775@163.com>
2021-02-22 21:14:23 +0800

    test connection

--- src/com/hjx/basic/HelloWorld.java
+++ src/com/hjx/basic/HelloWorld.java
@@ -1,7 +1,7 @@
 package com.hjx.basic;
 
 public class HelloWorld {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("This is a Hello World class! ");
     }
 }
